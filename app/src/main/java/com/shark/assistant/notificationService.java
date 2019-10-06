cedbe48c54f35becf43a2f2839427ce4f5aa1258
Sharkie <gillskk@gmail.com>
2019-10-06 18:37:18 +0100

    Update to Notification code

--- app/src/main/java/com/shark/assistant/notificationService.java
+++ app/src/main/java/com/shark/assistant/notificationService.java
@@ -1,11 +1,15 @@
 package com.shark.assistant;
 
+import android.app.Notification;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.support.v4.content.LocalBroadcastManager;
+import android.text.SpannableString;
 import android.util.Log;
 
 public class notificationService extends NotificationListenerService {
@@ -29,7 +33,25 @@ public class notificationService extends NotificationListenerService {
             String pack = sbn.getPackageName();
             Bundle extras = sbn.getNotification().extras;
             String title = extras.getString("android.title");
-            String text = extras.getString("android.text");
+
+            Object textObj = extras.get("android.text");
+
+            String text = null;
+            try{
+                if (textObj instanceof String){
+                    text = (String) textObj;
+                }else{
+                    SpannableString textString = (SpannableString) textObj;
+
+                    CharSequence charSeq = textString.subSequence(0, textString.length());
+
+                    text = charSeq.toString();
+                }
+
+
+            }catch(Exception e){
+                Log.wtf("textObj Error", e.toString());
+            }
 
             if (pack == null) pack = "";
             if (title == null) title = "";
